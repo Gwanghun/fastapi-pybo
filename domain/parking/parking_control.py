7a8d6ad5364e191aec1dbdafd85dcd3829fa7176
hooeni <hooeni@gmail.com>
2024-07-25 18:53:35 +0900

    크롬 드라이브 설치 옵션 수정

--- domain/parking/parking_control.py
+++ domain/parking/parking_control.py
@@ -32,8 +32,9 @@ class parking_control:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.binary_location = "/usr/bin/google-chrome"
 
-        service = Service(executable_path=ChromeDriverManager().install())
+        service = Service('/path/to/chromedriver')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
         self.driver.get(self.parking_url)
         time.sleep(2)
